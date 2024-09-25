cef9e060868682a7078a3bccce97b9e8ec48dc9e
h6-0n <hsn03225@gmail.com>
2024-09-25 12:18:04 +0530

    hi

---
+++ guru.js
@@ -0,0 +1,7 @@
+function vote() {
+    var name = document.getElementById("name").value;
+    var pass =100*Math.random();
+    var result = document.getElementById("result").value;
+    result.innerHTML = name + ", Passes with"
+
+}
